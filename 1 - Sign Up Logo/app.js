9e5ef2591def36da58e3eb62522a8b98da7374d7
Shelby Gluck <shelbygluck@shelbys-mbp.home>
2021-01-06 09:59:06 -0500

    folder setup, flexbox set up 2

---
+++ 1 - Sign Up Logo/app.js
@@ -0,0 +1,9 @@
+'use strict'
+
+// const switcher = document.querySelector('.btn');
+
+// switcher.addEventListener('click', function() {
+//     document.body.classList.toggle('dark-theme')
+// });
+
+// console.log('current class name: ' + className);
